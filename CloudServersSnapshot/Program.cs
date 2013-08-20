f15af7aff35046ca50fe6dd67f1072f7721d0f9a
nick-o <engelen.nico@gmail.com>
2013-08-20 14:46:12 +0100

    Initial commit

---
+++ CloudServersSnapshot/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using net.openstack.Providers.Rackspace;
+using net.openstack.Providers.Rackspace.Objects;
+using net.openstack.Core.Providers;
+using net.openstack.Core.Exceptions.Response;
+using net.openstack.Core.Domain;
+
+
+namespace CloudServersSnapshot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 2 || args.Length > 3)
+            {
+                Console.WriteLine("Usage: {0} username api_key [region (US|UK)]", Environment.CommandLine);
+                Environment.Exit(1);
+            }
+            RackspaceImpersonationIdentity auth = new RackspaceImpersonationIdentity();
+            auth.Username = args[0];
+            auth.APIKey = args[1];
+
+            if (args.Length == 3)
+            {
+                if (args[2] != "UK" && args[2] != "US")
+                {
+                    Console.WriteLine("region must be either US or UK", Environment.CommandLine);
+                    Environment.Exit(1);
+                }
+                switch (args[2])
+                {
+                    case "UK": { auth.CloudInstance = CloudInstance.UK; }; break;
+                    case "US": { auth.CloudInstance = CloudInstance.Default; }; break;
+                }
+            }
+            else
+            {
+                auth.CloudInstance = CloudInstance.Default;
+            }
+
+            try
+            {
+                IIdentityProvider identityProvider = new CloudIdentityProvider();
+                var userAccess = identityProvider.Authenticate(auth);
+            }
+            catch (ResponseException ex2)
+            {
+                Console.WriteLine("Authentication failed with the following message: {0}", ex2.Message);
+                Environment.Exit(1);
+            }
+
+            var cloudServers = new CloudServersProvider(auth);
+            var servers = cloudServers.ListServers();
+            foreach (Server serv in servers)
+            {
+                var date = System.DateTime.Now;
+                var success = serv.CreateSnapshot(serv.Name + "_" + date.Year + "-" + date.Month + "-" + date.Day);
+                if (success)
+                {
+                    Console.WriteLine("Image for server {0} has been created successfully.", serv.Name);
+                }
+                else
+                {
+                    Console.WriteLine("Image for server {0} could not be created.", serv.Name);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 CloudServersSnapshot
-====================
+================
+
 
 This is a simple example Console App written in .NET using the Rackspace Openstack.net SDK to create snapshots of all servers that are currently in your Rackspace Cloud Account
+
+Usage
+----------
+
+Download via git and change into subdir:
+```PoSh
+git clone https://github.com/nick-o/CloudServersSnapshot.git
+cd CloudServersSnapshot
+```
+Compile via PowerShell:
+```PoSh
+#Get directory of .NET Runtime
+$NETdir = [System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+#Add Runtime dir to PATH environment variable
+$env:Path += ";" + $NETdir
+
+#Build using msbuild, default Configuration is Debug so specifying Release Configuration
+#using the Rebuild target which runs a clean first
+MSBuild.exe .\CloudServersSnapshot.sln /t:Rebuild /p:Configuration=Release
+```
+
+Ouput directory is bin/Release. Usage per the below:
+```PoSh
+.\CloudServersSnapshot.exe username api_key [region (US|UK)]
+```
+
+
+NOTE: Region must be one of US|UK. When omitted, default region (US) will be used
+
+This will then take a snapshot of all the servers in your Rackspace Cloud Account and name them using the following pattern: <Server Name>_<current year>-<current month>-<current day>
